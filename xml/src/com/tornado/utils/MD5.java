9d20a06895944712efb1d0737d0fb0359c82e5c9
lizetor <lizetor@163.com>
2017-12-08 10:44:23 +0800

    v1.4

---
+++ xml/src/com/tornado/utils/MD5.java
@@ -0,0 +1,55 @@
+package com.tornado.utils;
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class MD5 {
+    //公盐
+    private static final String PUBLIC_SALT = "";
+
+    //十六进制下数字到字符的映射数组
+    private final static char md5String[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F' };
+
+    /**
+     * 用户密码加密，盐值为 ：私盐+公盐
+     */
+    public static String encryptPassword(String password, String salt) {
+        return encodeByMD5(PUBLIC_SALT + password + salt);
+    }
+
+    /**
+     * md5加密算法
+     */
+    private static String encodeByMD5(String originString) {
+
+        try {
+            //使用平台的默认字符集将此 String 编码为 byte序列，并将结果存储到一个新的 byte数组中
+            byte[] btInput = originString.getBytes();
+
+            //信息摘要是安全的单向哈希函数，它接收任意大小的数据，并输出固定长度的哈希值。
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+
+            //MessageDigest对象通过使用 update方法处理数据， 使用指定的byte数组更新摘要
+            mdInst.update(btInput);
+
+            // 摘要更新之后，通过调用digest（）执行哈希计算，获得密文
+            byte[] md = mdInst.digest();
+
+            // 把密文转换成十六进制的字符串形式
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {   //  i = 0
+                str[k++] = md5String[byte0 >>> 4 & 0xf];    //    5
+                str[k++] = md5String[byte0 & 0xf];   //   F
+            }
+
+            //返回经过加密后的字符串
+            return new String(str).toLowerCase();
+
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
